a8af411167bc128400338427f0d00e50466d0c64
Raj Manish <katrajmanish23@gmail.com>
2022-07-12 08:01:57 +0530

    Added a .cs file and did some editing with Rider.

---
+++ Test Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+    [SerializeField] private GameObject obj;
+    
+    private void Start() {
+        obj.transform.Rotate(Vector3.forward);
+    }
+
+    private void Update() {
+        obj.transform.Translate(Vector3.forward);
+    }
+}
